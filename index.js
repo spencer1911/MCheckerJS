72226edc020b645dc63d90719ec86b267d87b255
SynnKFPS <88551405+SynnKFPS@users.noreply.github.com>
2021-09-21 11:15:03 -0700

    More accurate now

--- index.js
+++ index.js
@@ -1,54 +1,38 @@
-/**
- * @author SynnK-FPS
- * @description Small version of old SCheckerRevamp/JS
- */
-
 const fs = require('fs');
 const readline = require('readline');
 const request = require('request');
 const chalk = require('chalk');
-const { yellow } = require('chalk');
 
 const rl = readline.createInterface({
-    input: fs.createReadStream('usernames.txt'),
+    input: fs.createReadStream('./usernames.txt'),
     output: process.stdout,
-    terminal: false
+    terminal: false,
+    readable: true
 });
-
+ 
 rl.on('line', (line) => {
-    request('https://api.mojang.com/users/profiles/minecraft/' + line, function(body) {
-
-        if (line.length <= 2) {
-            console.log(chalk.yellow("A line contains a username with 2 letters or less."))
-            fs.appendFile('./invalid.txt', `${line.toString()}, <----- 2 Letters or less \n`, function(){})
-        } else
-        
-        if (line.length >= 17) {
-            console.log(chalk.yellow("A line contains a username with 17 letters or more."))
-            fs.appendFile('./invalid.txt', `${line.toString()}, <----- More than 16 letters \n`, function(){})
-        } else 
-
-        if (line.includes(` `)) {
-            console.log(chalk.yellow("A line contains a invalid symbol."))
-            fs.appendFile('./invalid.txt', `${line.toString()}, <----- Invalid Symbol \n`, function(){})
-        } else
-        
-        if (String(body).includes("TooManyRequestsException")) {
-            console.log(yellow("Rate-limit"))
-            fs.appendFile('./invalid.txt', `${line.toString()}, <----- Rate-limited \n`, function(){})
-        } else
-
-        if (String(body).includes("Requested")) {
-            console.log(yellow("Invalid Symbol Request"))
-            fs.appendFile('./invalid.txt', `${line.toString()}, <----- Invalid Symbol \n`, function(){})
-        } else
-
-        if (String(body).startsWith('{"name":')) {
-            fs.appendFile('./unavailables.txt', `${line}, \n`, function(){})
-            console.log(chalk.red(`Username ${line} is already in use.`))
-        } else {
-            fs.appendFile('./availables.txt', `${line}, \n`, function(){})
-            console.log(chalk.green(`Username ${line} is available.`))
+    request('https://api.mojang.com/users/profiles/minecraft/' + line, function (err, res, body) {
+        if (String(body) == "") {
+            if (line.length <= 2) {
+                fs.appendFile('./invalids.txt', `${line}, <----- 2 or less letters. \n`, function(){})
+                console.log(chalk.magentaBright(`${line} is invalid.`))
+            } else {
+                fs.appendFile('./availables.txt', `${line}, \n`, function(){})
+                console.log(chalk.greenBright(`${line} is available.`))
+            } 
         }
-    })
-});
+
+        if (String(body).includes(`"TooManyRequestsException"`)) {
+            console.log(chalk.bgGrey(`Username ${line} was rate-limited.`))
+        }
+
+        if (String(body).includes(`"id"`) == true) {
+            console.log(chalk.red(`${line} is already in use.`))
+        }
+
+        if (String(body).includes(`"BadRequestException"`)) {
+            fs.appendFile('./invalids.txt', `${line}, <----- ${line.length} letters \n`, function(){})
+            console.log(chalk.gray(`${line} has more than 16 letters. (${line.length} letters)`))
+        }
+    });
+})
